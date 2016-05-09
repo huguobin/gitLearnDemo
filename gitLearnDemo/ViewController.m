cce10773ad750587fff4489ac7e96b7d3bab8080
hugb <1154684853@qq.com>
2016-05-09 16:05:16 +0800

    resolve buge

--- gitLearnDemo/ViewController.m
+++ gitLearnDemo/ViewController.m
@@ -23,7 +23,7 @@
     [self.view addSubview:lable];
     
     UILabel * lable2 = [[UILabel alloc]initWithFrame:CGRectMake(200, 100, 100, 30)];
-    lable2.text=@"异地测试";
+    lable2.text=@"Bug测试";
     lable2.textColor=[UIColor purpleColor];
     [self.view addSubview:lable2];
    
